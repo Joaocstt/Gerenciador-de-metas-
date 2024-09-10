d23be44b5f681afd1281dbe764a602f86715f001
unknown <jnc201820@gmail.com>
2024-09-10 18:53:56 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const { select, input } = require('@inquirer/prompts');
+
+let meta = {
+    value: "Tomar 3L de água por dia",
+    checked: false,
+};
+
+let metas = [ meta ]
+
+
+
+const cadastrarMeta = async () => {
+    const metaInput = await input({message: "Digite a meta: "});
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vázia.");
+        return;
+    }
+    
+    metas.push({
+      value: metaInput,
+      checked: false,
+    });
+}
+
+const start = async () => {
+
+    while (true) {
+              
+    const opcao = await select({
+        message: "Menu >",
+        choices: [
+            {
+                name: "Cadastrar meta",
+                value: "cadastrar"
+            },
+            {
+                name: "Listar metas",
+                name: "listar" 
+            },
+            {
+                name: "Sair",
+                value: "sair"
+            }
+        ]
+    }) 
+
+    switch (opcao) {
+        case "cadastrar":
+            await cadastrarMeta();
+            console.log(metas)
+        break;
+        case "listar":
+            console.log("Vamos listar");
+            break;
+        case "sair":
+            console.log("Até a próxima");
+            return;
+    }
+  }
+};
+
+start();
